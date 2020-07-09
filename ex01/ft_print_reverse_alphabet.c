8d07f3c7a247a6cf365209d0c3e7d12724c6ee89
hathabanJHB <hathaban@c5r6s2.wethinkcode.co.za>
2020-07-09 12:18:17 +0200

    ex 00 -2

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hathaban <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 10:16:06 by hathaban          #+#    #+#             */
+/*   Updated: 2020/07/09 12:05:43 by hathaban         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_reverse_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'z';
+	while (alpha >= 'a')
+	{
+		ft_putchar(alpha);
+		--alpha;
+	}
+}
